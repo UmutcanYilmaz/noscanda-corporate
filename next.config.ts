ad2117b79a066058232a858d4f063c7d06712109
UMUTCAN YILMAZ <contact@umutcanyilmaz.com>
2026-07-21 22:01:45 +0300

    chore: comment out standalone output configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  // output: 'standalone',
   images: {
     unoptimized: false,
     formats: ['image/avif', 'image/webp'],
